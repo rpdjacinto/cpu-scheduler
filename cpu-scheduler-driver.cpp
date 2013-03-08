be946c2e7fbf94137be219bddf656caad2414545
isha-l <isha_garg_92@yahoo.com>
2013-03-08 12:55:09 -0800

    some error checking

--- cpu-scheduler-driver.cpp
+++ cpu-scheduler-driver.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 
-
 void printHelp()
 {
 	cout<<"\ncpu-scheduler: A post-mortem utility designed to analyze the quality of CPU-scheduling algorithms.";
 	cout<<"\nUsage:";
 	cout<<"\n\tcpusched -w [text file with workload] -a [scheduling algorithm]"; 
+	cout<<"\n\t\t[-p] parameter for algorithm (if required)";
 	cout<<"\n\t\t[-h] print help";
 	cout<<"\n\t\t[-v] verbose";
 }
@@ -46,11 +46,27 @@ int main(int argc, char *argv[])
 	}
 
 
-
+	/* Print help information
+	 */
 	if (help == 1)
 	{
 		printHelp();
 	}
+
+	/* Error checking
+	 */
+	else if (workload.empty() || scheduling_algorithm.empty())
+	{
+		cout<<"\nIncomplete arguments. See 'cpusched -h' for usage.";
+	}
+
+	/* Main processing - parse workload file, initialize algorithm object
+	 */
+	else
+	{
+
+	}
+
 
 
 	return 0;
