69f38bc8103d837e3553656052e518f86fc8a56a
unknown <mackenzieajudd@github.com>
2013-03-14 19:04:35 -0700

    Added a header file for pcb class.

---
+++ pcb.h
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+class Pcb {
+
+public:
+
+private:
+	int pid;
+	int tarq;
+	int priority;
+	int cpuBursts;
+	vector<int> cpuBursts;
+	vector<int> ioBursts;
+	
+};
