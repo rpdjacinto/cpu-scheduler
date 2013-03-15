a937f8060f075efc9d8e178cdf6f082874a8af62
unknown <mackenzieajudd@github.com>
2013-03-14 19:09:56 -0700

    Changed the pcb.cpp file

--- Pcb.cpp
+++ Pcb.cpp
@@ -1,16 +1,1 @@
-#include <vector>
-
-using namespace std;
-
-class Pcb {
-
-public:
-
-private:
-	int pid;
-	int tarq;
-	int priority;
-	int cpuBursts;
-	vector<int> cpuBursts;
-	vector<int> ioBursts;
-};
+#include "pcb.h"
