a2649fa7878c48215f5866cf649ca3cd844c2cc9
Jerry Huang <jerryhuang0123@hotmail.com>
2013-03-18 13:47:48 -0700

    separate SJF .cpp file just in case

---
+++ shortest-job-first2.cpp
@@ -0,0 +1,45 @@
+#include "shortest-job-first2.h"
+#include <string>
+#include <string.h>
+
+ShortestJobFirst::ShortestJobFirst(vector<Pcb> processes)
+{
+          setProcesses(processes);
+	        setInactiveProcesses(processes);
+	        setWaitingQueue(processes);
+	        setTime(0);
+}
+
+int ShortestJobFirst::selectProcess() 
+{
+	        vector<Pcb> readyQueue = getReadyQueue();
+
+	        int pidOfSelectedProcess = -1; 
+		
+		if( readyQueue.size() > 0 ) 
+		{
+	                pidOfSelectedProcess = 0;
+					int pidOfEqualProcess = 9999;
+	                
+			int shortestBurst = readyQueue.front().getCpuBurst(readyQueue.front().getCurrentCpuBurst());
+	                
+			for( int i = 0; i < readyQueue.size(); i++) 
+			{
+	                        if( readyQueue[i].getCpuBurst(readyQueue[i].getCurrentCpuBurst()) < shortestBurst ) {
+	                                shortestBurst = readyQueue[i].getCpuBurst(readyQueue[i].getCurrentCpuBurst());
+	                                pidOfSelectedProcess = readyQueue[i].getPid();
+	                        } // if
+							else if (readyQueue[i].getCpuBurst(readyQueue[i].getCurrentCpuBurst()) = shortestBurst ) {
+								pidOfEqualProcess = readyQueue[i].getPid();
+							} //else if
+			}
+			if (pidOfEqualProcess < pidOfSelectedProcess){
+				pidOfSelectedProcess = pidOfEqualProcess;
+	        } //if
+	        } //if
+		else{
+			cout << "No processes in the ready Queue \n";
+		} //else
+		return pidOfSelectedProcess;
+}
+
