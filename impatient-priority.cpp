2ed9146f4e20ba3e24d025954ed4b11e45aac200
MacKenzie Judd <mackenzieajudd@gmail.com>
2013-03-20 10:31:53 -0700

    Impatient priority working.

--- impatient-priority.cpp
+++ impatient-priority.cpp
@@ -1,4 +1,4 @@
-#include "impatient-priority.h"
+#include "INCLUDES/impatient-priority.h"
 
 using namespace std;
 
@@ -10,19 +10,34 @@ ImpatientPriority::ImpatientPriority( vector<Pcb> processes ) : SchedulingAlgori
 
 int ImpatientPriority::selectProcess() {
 	vector<Pcb> readyQueue = getReadyQueue();
-	int highestPriority = readyQueue[0].getPriority();
 	int selectedProcessIndex = 0;
-	for( int i = 0; i < readyQueue.size(); i++ ) {
-		if( readyQueue[i].getPriority() < highestPriority ) {
-			highestPriority = readyQueue[i].getPriority();
-			selectedProcessIndex = i;
+	selectedProcessIndex = 0;
+	int highestPriority;
+	if(readyQueue.size() != 0){
+		highestPriority = readyQueue[0].getPriority();
+	
+		for( int i = 0; i < readyQueue.size(); i++ ) {
+			if( readyQueue[i].getPriority() < highestPriority ) {
+				highestPriority = readyQueue[i].getPriority();
+				selectedProcessIndex = i;
+			}
+		}
+
+		if( readyQueue[selectedProcessIndex].getPriority() < getCurrentProcess().getPriority() || isCurrentProcessSet == false) {
+
+			if(isCurrentProcessSet != false)
+				readyQueue.push_back( getCurrentProcess() );
+			setCurrentProcess( readyQueue[selectedProcessIndex] );
+			readyQueue.erase( readyQueue.begin() + selectedProcessIndex );
+			isCurrentProcessSet = true;
+		
 		}
 	}
-	if( readyQueue[selectedProcessIndex].getPriority() < getCurrentProcess().getPriority() ) {
-		setCurrentProcess( readyQueue[selectedProcessIndex] );
-		readyQueue.erase( readyQueue.begin() + selectedProcessIndex );
-		setReadyQueue( readyQueue );
-	}
+	
+	
+
+	setReadyQueue( readyQueue );
+
 	return 0;
 }
 
