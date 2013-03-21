7ca6fffbed2a34ecad9366fb34712fb0aac5bbce
Rafael Jacinto <rpdjacinto@gmail.com>
2013-03-20 21:33:02 -0700

    Ageing complete!

--- priority-npr.cpp
+++ priority-npr.cpp
@@ -13,28 +13,27 @@ PriorityNpr::PriorityNpr( vector<Pcb> processes ) : SchedulingAlgorithm(processe
 }
 
 int PriorityNpr::selectProcess() {
-	vector<Pcb> vipQueue = this->vipQueue;
 	vector<Pcb> readyQueue = getReadyQueue();
 	for( int i = 0; i < readyQueue.size(); i++ ) {
-		if( readyQueue[i].getWaitingTime() >= this->threshold ) {
-			vipQueue.push_back( readyQueue[i] );
+		if( readyQueue[i].getCurrentWaitingTime() >= this->threshold ) {
+			this->vipQueue.push_back( readyQueue[i] );
 			readyQueue.erase( readyQueue.begin() + i );
 			setReadyQueue( readyQueue );
 		}
 	}
 	if (isCurrentProcessSet == false){
-		if( vipQueue.size() > 0 ) {
-			int highestPriority = vipQueue[0].getPriority();
+		if( this->vipQueue.size() > 0 ) {
+			int highestPriority = this->vipQueue[0].getPriority();
 			int selectedProcessIndex = 0;
-			for( int i = 0; i < vipQueue.size(); i++ ) {
-				if( vipQueue[i].getPriority() < highestPriority ) {
-					highestPriority = vipQueue[i].getPriority();
+			for( int i = 0; i < this->vipQueue.size(); i++ ) {
+				if( this->vipQueue[i].getPriority() < highestPriority ) {
+					highestPriority = this->vipQueue[i].getPriority();
 					selectedProcessIndex = i;
 				}
 			}
-			setCurrentProcess( readyQueue[selectedProcessIndex] );
-			readyQueue.erase( readyQueue.begin() + selectedProcessIndex );
-			setReadyQueue( readyQueue );
+			this->vipQueue[selectedProcessIndex].setCurrentWaitingTime(0);
+			setCurrentProcess( this->vipQueue[selectedProcessIndex] );
+			this->vipQueue.erase( this->vipQueue.begin() + selectedProcessIndex );
 			isCurrentProcessSet = true;
 			return 0;
 		} else if( readyQueue.size() > 0 ) {
@@ -46,6 +45,7 @@ int PriorityNpr::selectProcess() {
 					selectedProcessIndex = i;
 				}
 			}
+			readyQueue[selectedProcessIndex].setCurrentWaitingTime(0);
 			setCurrentProcess( readyQueue[selectedProcessIndex] );
 			readyQueue.erase( readyQueue.begin() + selectedProcessIndex );
 			setReadyQueue( readyQueue );
